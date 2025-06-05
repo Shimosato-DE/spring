c164c7b0899c617e9209bdad16554b041f33e134
kinok <kinok@DESKTOP-09A47AU>
2025-06-05 19:13:34 +0900

    Spring研修_step6

---
+++ spring_step6/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,42 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.example.demo.entity.User;
+import com.example.demo.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class UserController {
+	
+	private final UserService userService;
+	
+	@GetMapping("/user/list")
+	public String displayList(Model model) {
+		List<User> userlist = userService.serchAll();
+		
+		model.addAttribute("userlist", userlist);
+		
+		return "/user/list";
+	}
+	
+	@GetMapping("/user/add")
+	public String displayAdd() {
+		return "/user/add";
+	}
+	
+
+	@GetMapping("/user/{id}")
+	public String displayView(@PathVariable Long id, Model model) {
+		User user = userService.findById(id);
+		model.addAttribute("userData", user);
+		return "/user/view";
+	}
+}
